cf56d3b19fd631ba766a59fa414ccd335b870785
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-02-28 15:53:07 +0800

    Update diy-J.sh

--- diy-J.sh
+++ diy-J.sh
@@ -44,8 +44,8 @@ cp $CURRENT_DIR/DIY/app_bg_black.png $CURRENT_DIR/$DIR/app/src/main/res/drawable
 # 默认设置
 cp $CURRENT_DIR/DIY/J/App.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/base/App.java 
 
-# 内置接口String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/zhoujck/tv/raw/master/box");
-#sed -i 's#API_URL, ""#API_URL, "https://gitee.com/zhoujck/tv/raw/master/box"#g'   $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+# 内置接口
+sed -i 's#API_URL, ""#API_URL, "https://gitee.com/zhoujck/tv/raw/master/box"#g'   $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
 
 #自定义epg
 #cp $CURRENT_DIR/DIY/epg_data.json $CURRENT_DIR/$DIR/app/src/main/assets/epg_data.json
