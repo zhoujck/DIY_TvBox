b4dda538dc755cbbab3317a477143cd90a36775a
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-06 14:58:21 +0800

    Update HomeActivity.java

--- DIY/J/HomeActivity.java
+++ DIY/J/HomeActivity.java
@@ -273,7 +273,7 @@ public class HomeActivity extends BaseActivity {
                         try { 
                             if (paramString.contains("hitokoto")) {
                                 JSONObject jsonObject = new JSONObject (paramString);
-                                String value = jsonObject.optString("hitokoto");
+                                String value = jsonObject.optString("hitokoto") + " ——" +jsonObject.optString("from_who");
                                 tvtalk1.setText(value);
                              } else {
                                      tvtalk1.setText(ApiConfig.get().tvtalk); 
