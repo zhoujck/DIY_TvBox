c56e7c9482f69fa755f4ef0e2e7d75fccfde6a06
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-13 21:49:26 +0800

    Update LivePlayActivity.java

--- DIY/J/LivePlayActivity.java
+++ DIY/J/LivePlayActivity.java
@@ -442,7 +442,7 @@ public class LivePlayActivity extends BaseActivity {
                     if (paramString.contains("epg_data")) {
                         final JSONArray jSONArray = new JSONObject(paramString).optJSONArray("epg_data");
                         if (jSONArray != null)
-                            for (int b = 0; b < jSONArray.length(); b++) {
+                            for (int b = -1; b < jSONArray.length(); b++) {
                                 JSONObject jSONObject = jSONArray.getJSONObject(b);
                                 //Epginfo epgbcinfo = new Epginfo(jSONObject.optString("title"), jSONObject.optString("start"), jSONObject.optString("end"));
                                 Epginfo epgbcinfo = new Epginfo(date,jSONObject.optString("title"), date, jSONObject.optString("start"), jSONObject.optString("end"),b);
@@ -559,7 +559,7 @@ public class LivePlayActivity extends BaseActivity {
             liveIconNullBg.setVisibility(View.VISIBLE);
             liveIconNullText.setVisibility(View.VISIBLE);
             imgLiveIcon.setVisibility(View.INVISIBLE);
-            liveIconNullText.setText("" + channel_Name.getChannelNum());
+            //liveIconNullText.setText("" + channel_Name.getChannelNum());
         } else {
             imgLiveIcon.setVisibility(View.VISIBLE);
             Picasso.get().load(logoUrl).placeholder(R.drawable.app_banner).into(imgLiveIcon);
