9d8ecc0d0e9e14bec890e781afdc9f103ad0f5bb
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-14 20:14:34 +0800

    Update LivePlayActivity.java

--- DIY/J/LivePlayActivity.java
+++ DIY/J/LivePlayActivity.java
@@ -481,7 +481,7 @@ public class LivePlayActivity extends BaseActivity {
                 ArrayList arrayList = (ArrayList) hsEpg.get(savedEpgKey);
                 if (arrayList != null && arrayList.size() > 0) {
                     int size = arrayList.size() - 1;
-                    while (size >= 0) {
+                    while (size >= 0); {
                         if (new Date().compareTo(((Epginfo) arrayList.get(size)).startdateTime) >= 0) {
                             tip_epg1.setText(((Epginfo) arrayList.get(size)).start + "--" + ((Epginfo) arrayList.get(size)).end);
                             ((TextView) findViewById(R.id.tv_current_program_name)).setText(((Epginfo) arrayList.get(size)).title);
@@ -510,7 +510,7 @@ public class LivePlayActivity extends BaseActivity {
             }
             
 
-            if(!tip_epg1.getText().equals("暂无信息")){
+           // if(!tip_epg1.getText().equals("暂无信息")){
                 ll_epg.setVisibility(View.VISIBLE);
                 countDownTimer = new CountDownTimer(6000, 1000) {//底部epg隐藏时间设定
                     public void onTick(long j) {
@@ -520,9 +520,9 @@ public class LivePlayActivity extends BaseActivity {
                     }
                 };
                 countDownTimer.start();
-            }else {
-                ll_epg.setVisibility(View.GONE);
-            }
+          //  }else {
+          //     ll_epg.setVisibility(View.GONE);
+          //  }
             
             if (channel_Name == null || channel_Name.getSourceNum() <= 0) {
                 ((TextView) findViewById(R.id.tv_source)).setText("1/1");
