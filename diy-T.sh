51bebac6ffa4a72379b3a369a6c62ae11fa3036e
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-17 21:20:33 +0800

    Update diy-T.sh

--- diy-T.sh
+++ diy-T.sh
@@ -30,7 +30,7 @@ mv $CURRENT_DIR/DIY/app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg_black.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
 
-# 主页UI调整
+# 主页UI调整 恢复老版；默认多行显示
 cp $CURRENT_DIR/DIY/T/fragment_user.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_user.xml
 
 # 主页增加每日一言/去除部分图标
