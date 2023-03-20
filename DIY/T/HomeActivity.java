8d13d81308a15ef0ac3c4b27639efe58123803fb
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-20 11:30:17 +0800

    Update HomeActivity.java

--- DIY/T/HomeActivity.java
+++ DIY/T/HomeActivity.java
@@ -617,6 +617,9 @@ public class HomeActivity extends BaseActivity {
                 } else {
                     exit();
                 }
+              } else if (baseLazyFragment instanceof UserFragment && UserFragment.tvHotListForGrid.canScrollVertically(-1)) {
+                UserFragment.tvHotListForGrid.scrollToPosition(0);
+                this.mGridView.setSelection(0);   
             } else {
                 exit();
             }
@@ -692,7 +695,7 @@ public class HomeActivity extends BaseActivity {
         boolean activated = count > 0;
         currentView.findViewById(R.id.tvFilter).setVisibility(View.VISIBLE);
         ImageView imgView = currentView.findViewById(R.id.tvFilter);
-        imgView.setColorFilter(activated?this.getThemeColor(): Color.WHITE);
+        imgView.setColorFilter(activated ?this.getThemeColor(): Color.WHITE);
     }
 
     private final Runnable mDataRunnable = new Runnable() {
