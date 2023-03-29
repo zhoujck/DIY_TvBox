70e59e46e39420a937ea22a781addc8e7e501151
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-29 14:15:32 +0800

    Update HomeActivity.java

--- DIY/T/HomeActivity.java
+++ DIY/T/HomeActivity.java
@@ -388,7 +388,7 @@ public class HomeActivity extends BaseActivity {
         
        tvtalk1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onLongClick(View v) {
+            public boolean onLongClick(View v) {
                 //获取剪切板管理器
                 ClipboardManager cm = (ClipboardManager)getSystemService(mContext.CLIPBOARD_SERVICE);
                 //设置内容到剪切板
