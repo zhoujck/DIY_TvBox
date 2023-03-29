18fef8afdd16447a4ceebd2fba0100fc0968de7c
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-03-29 17:53:55 +0800

    Update HomeActivity.java

--- DIY/T/HomeActivity.java
+++ DIY/T/HomeActivity.java
@@ -393,7 +393,7 @@ public class HomeActivity extends BaseActivity {
                 ClipboardManager cm = (ClipboardManager)getSystemService(mContext.CLIPBOARD_SERVICE);
                 //设置内容到剪切板
                 cm.setPrimaryClip(ClipData.newPlainText(null, tvtalk1.getText().toString().replace("新版地址点击复制 ","")));
-                Toast.makeText(HomeActivity.this, "已复制，快去下载吧！", Toast.LENGTH_SHORT).show();
+                Toast.makeText(HomeActivity.this, "已复制到剪切板！", Toast.LENGTH_SHORT).show();
             }
         });
 
